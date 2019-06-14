8c3c2774a57894cd13093b96b81e60e4aa739697
Pablo Gracia <pablo.graciag@gmail.com>
2019-06-14 13:02:57 +0200

    All basic logic implemented. Tests passed.

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,6 +8,9 @@ import { willBeAlive } from "./index";
 import { numberAliveNeighbours } from "./index";
 import { getMatrix } from "./index";
 import { analyzeBorders } from "./index";
+import { reduceMatrix } from "./index";
+import { enlargeMatrix } from "./index";
+import { createNewWorld } from "./index";
 
 test("willBeAlive_1", () => {
   expect(willBeAlive(true, 4)).toBe(false);
@@ -69,6 +72,16 @@ test("numberAliveNeighbours_5", () => {
   ).toBe(undefined);
 });
 
+test("numberAliveNeighbours_5", () => {
+  expect(
+    numberAliveNeighbours([
+      [false, false, true],
+      [false, true, true],
+      [false, false, false]
+    ])
+  ).toBe(2);
+});
+
 test("getMatrix_1", () => {
   expect(
     getMatrix(
@@ -98,6 +111,26 @@ test("getMatrix_2", () => {
       2
     )
   ).toEqual([[false, false, true], [true, true, true], [true, false, false]]);
+});
+
+test("getMatrix_3", () => {
+  expect(
+    getMatrix(
+      [
+        [false, true, false, false, false],
+        [false, true, true, false, false],
+        [true, true, false, true, false],
+        [false, false, false, true, false],
+        [false, true, true, true, false]
+      ],
+      1,
+      3
+    )
+  ).toEqual([
+    [false, false, false],
+    [true, false, false],
+    [false, true, false]
+  ]);
 });
 
 test("analyzeBorders_1", () => {
@@ -151,4 +184,85 @@ test("analyze_borders_6", () => {
   expect(
     analyzeBorders([[true, false, true], [false, false], [true, true, false]])
   ).toBe(undefined);
+});
+
+test("reduce_matrix_1", () => {
+  expect(
+    reduceMatrix([
+      [false, false, false, false],
+      [],
+      [false, false, false, false]
+    ])
+  ).toBe(undefined);
+});
+
+test("reduce_matrix_2", () => {
+  expect(
+    reduceMatrix([
+      [false, false, false, false],
+      [false, false, false, false],
+      [false, false, false, false],
+      [false, false, false, false]
+    ])
+  ).toEqual([[false, false], [false, false]]);
+});
+
+test("reduce_matrix_3", () => {
+  expect(
+    reduceMatrix([
+      [false, false, true, true, false],
+      [true, true, false, true, true],
+      [true, true, true, true, true],
+      [false, false, true, false, false],
+      [true, false, true, true, true]
+    ])
+  ).toEqual([[true, false, true], [true, true, true], [false, true, false]]);
+});
+
+test("reduce_matrix_4", () => {
+  expect(reduceMatrix([[true, false], [false, true]])).toBe(undefined);
+});
+
+test("enlarge_matrix_1", () => {
+  expect(
+    enlargeMatrix([
+      [true, false, true],
+      [false, true, false],
+      [false, false, true]
+    ])
+  ).toEqual([
+    [false, false, false, false, false],
+    [false, true, false, true, false],
+    [false, false, true, false, false],
+    [false, false, false, true, false],
+    [false, false, false, false, false]
+  ]);
+});
+
+test("enlarge_matrix_2", () => {
+  expect(enlargeMatrix([[true, false, true], []])).toBe(undefined);
+});
+
+test("create_new_world_1", () => {
+  expect(createNewWorld([[]])).toBe(undefined);
+});
+
+test("create_new_world_2", () => {
+  expect(
+    createNewWorld([
+      [false, true, false, false, false],
+      [false, true, true, false, false],
+      [true, true, false, true, false],
+      [false, false, false, true, false],
+      [false, true, true, true, false]
+    ])
+  ).toEqual([
+    [false, false, false, false, false, false, false],
+    [false, true, false, true, false, false, false],
+    [false, false, false, false, true, false, false],
+    [false, false, true, false, false, true, false],
+    [false, true, false, false, true, true, false],
+    [false, false, false, true, false, true, false],
+    [false, false, true, true, true, false, false]
+  ]);
 });
--- src/index.ts
+++ src/index.ts
@@ -84,6 +84,11 @@ export const getMatrix = (
   return objective_matrix;
 };
 
+/**
+ * Function analyzeBorders
+ * Checks whether some alive cell is in the border of the world. Returns true if the are cells on the border, false if not
+ * @param world_matrix boolean nxm matrix representing the world
+ */
 export const analyzeBorders = (
   world_matrix: boolean[][]
 ): boolean | undefined => {
@@ -117,4 +122,143 @@ export const analyzeBorders = (
   }
 
   return false;
+};
+
+/**
+ * Function reduceMatrix
+ * Reduces the provided matrix by removing the four borders. Returns the reduced matrix.
+ * @param world_matrix boolean nxm matrix representing the world
+ */
+export const reduceMatrix = (
+  world_matrix: boolean[][]
+): boolean[][] | undefined => {
+  // Check that the length of the rows is the same for all of them:
+  for (let i = 1; i < world_matrix.length; i++) {
+    if (world_matrix[i].length !== world_matrix[0].length) {
+      console.log("The rows don't have the same length. F(reduceMatrix)");
+      return undefined;
+    }
+  }
+
+  // Check world_matrix is of at least 3x3 size:
+  if (world_matrix.length < 3) {
+    console.log("The provided matrix is too small");
+    return undefined;
+  } else if (world_matrix[0].length < 3) {
+    console.log("The provided matrix is too small");
+    return undefined;
+  }
+
+  const number_rows: number = world_matrix[0].length;
+  const number_columns: number = world_matrix.length;
+
+  let new_world_matrix: boolean[][] = [];
+
+  // Reduce the original matrix
+  for (let i = 1; i < number_columns - 1; i++) {
+    new_world_matrix.push(world_matrix[i]);
+    new_world_matrix[i - 1].splice(0, 1);
+    new_world_matrix[i - 1].pop();
+  }
+
+  return new_world_matrix;
+};
+
+/**
+ * Function enlargeMatrix
+ * Enlarges the world matrix by adding a new border to each side containing false values
+ * @param world_matrix boolean nxm matrix representing the world
+ */
+export const enlargeMatrix = (
+  world_matrix: boolean[][]
+): boolean[][] | undefined => {
+  // Check that the length of the rows is the same for all of them:
+  for (let i = 1; i < world_matrix.length; i++) {
+    if (world_matrix[i].length !== world_matrix[0].length) {
+      console.log("The rows don't have the same length. F(enlargeMatrix)");
+      return undefined;
+    }
+  }
+
+  // Check world_matrix is of at least 3x3 size:
+  if (world_matrix.length < 3) {
+    console.log("The provided matrix is too small. F(enlargeMatrix)");
+    return undefined;
+  } else if (world_matrix[0].length < 3) {
+    console.log("The provided matrix is too small. F(enlargeMatrix)");
+    return undefined;
+  }
+
+  const new_world_matrix: boolean[][] = [];
+  let new_row: boolean[] = [];
+  for (let _ = 0; _ < world_matrix.length + 2; _++) {
+    new_row.push(false);
+  }
+  new_world_matrix.push(new_row);
+  for (let y = 0; y < world_matrix.length; y++) {
+    new_world_matrix.push([false]);
+    for (let x = 0; x < world_matrix[0].length; x++) {
+      new_world_matrix[y + 1].push(world_matrix[y][x]);
+    }
+    new_world_matrix[y + 1].push(false);
+  }
+  new_world_matrix.push(new_row);
+
+  return new_world_matrix;
+};
+
+export const createNewWorld = (
+  world_matrix: boolean[][]
+): boolean[][] | undefined => {
+  // Check that the length of the rows is the same for all of them:
+  for (let i = 1; i < world_matrix.length; i++) {
+    if (world_matrix[i].length !== world_matrix[0].length) {
+      console.log("The rows don't have the same length. F(createNewWorld)");
+      return undefined;
+    }
+  }
+
+  // Check world_matrix is of at least 3x3 size:
+  if (world_matrix.length < 3) {
+    console.log("The provided matrix is too small. F(createNewWorld)");
+    return undefined;
+  } else if (world_matrix[0].length < 3) {
+    console.log("The provided matrix is too small. F(createNewWorld)");
+    return undefined;
+  }
+
+  let edited_world_matrix: boolean[][];
+  let new_world_matrix: boolean[][] = [];
+  let new_row: boolean[] = [];
+
+  if (analyzeBorders(world_matrix)) {
+    edited_world_matrix = enlargeMatrix(enlargeMatrix(world_matrix));
+  } else {
+    edited_world_matrix = enlargeMatrix(world_matrix);
+  }
+
+  for (let _ = 0; _ < edited_world_matrix[0].length; _++) {
+    new_row.push(false);
+  }
+
+  new_world_matrix.push(new_row);
+  for (let x = 1; x < edited_world_matrix.length - 1; x++) {
+    // console.log("y", y);
+    new_world_matrix.push([false]);
+    for (let y = 1; y < edited_world_matrix[0].length - 1; y++) {
+      new_world_matrix[x].push(
+        willBeAlive(
+          edited_world_matrix[x][y],
+          numberAliveNeighbours(getMatrix(edited_world_matrix, x, y))
+        )
+      );
+    }
+    new_world_matrix[x].push(false);
+  }
+  new_world_matrix.push(new_row);
+
+  while (!analyzeBorders(new_world_matrix)) {
+    new_world_matrix = reduceMatrix(new_world_matrix);
+  }
+  return new_world_matrix;
 };
